c49e282fe3fcdf8e112a074eb76e00ecfe196c93
ahmed mohamed <ahmed_291023@outlook.com>
2024-07-25 06:02:16 +0300

    modify color palette

--- app.js
+++ app.js
@@ -1,44 +1,38 @@
-let mainButtons = Array.from(document.querySelectorAll(".container .auth .message button"))
-let forms = Array.from(document.forms)
-let passwords = Array.from(document.querySelectorAll(".password"))
-let showPassword = Array.from(document.querySelectorAll(".show-password"))
-let container = document.querySelector(".container")
-let subContainer = document.querySelector(".container .auth")
-let signupContainer = document.querySelector(".container .sign-up")
-let loginContainer = document.querySelector(".container .login")
-let errorMessages = Array.from(document.querySelectorAll(".container .error-message"))
-let emailInputs = Array.from(document.querySelectorAll("input[type='email']"))
-let allInputs = Array.from(document.querySelectorAll("input"))
-let forgetPasswordButton = document.querySelector("a")
-let forgetPasswordDiv = document.querySelector(".forget-password-div")
-let loginDiv = document.querySelector(".login-form > div")
-let loginForm = document.querySelector('form.login-form')
-let sendCodeBtn = document.querySelector('.send-code')
-
+// Selectors
+const mainButtons = Array.from(document.querySelectorAll(".container .auth .message button"))
+const forms = Array.from(document.forms)
+const passwords = Array.from(document.querySelectorAll(".password"))
+const showPassword = Array.from(document.querySelectorAll(".show-password"))
+const container = document.querySelector(".container")
+const subContainer = document.querySelector(".container .auth")
+const signupContainer = document.querySelector(".container .sign-up")
+const loginContainer = document.querySelector(".container .login")
+const errorMessages = Array.from(document.querySelectorAll(".container .error-message"))
+const emailInputs = Array.from(document.querySelectorAll("input[type='email']"))
+const allInputs = Array.from(document.querySelectorAll("input"))
+const forgetPasswordButton = document.querySelector("a")
+const forgetPasswordDiv = document.querySelector(".forget-password-div")
+const loginDiv = document.querySelector(".login-form > div")
+const loginForm = document.querySelector('form.login-form')
+const sendCodeBtn = document.querySelector('.send-code')
+const otpForm = document.querySelector('.otp')
+const otpInputs = document.querySelectorAll("input[name='otp']")
+const otpButton = document.querySelector('.otp button')
+const retriveEmailInput = document.getElementById('retrive-email') 
+const paletteSwitcher1 = document.querySelector('.palette-switcher-1');
+const paletteSwitcher2 = document.querySelector('.palette-switcher-2');
 const emailRegex = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+const palettes = ['palette-1', 'palette-2', 'palette-3', 'palette-4', 'palette-5'];
 
+// Logic to prevent default behaviour of invalid inputs
 allInputs.forEach((input)=> {
-    input.addEventListener('invalid', (e)=>{
-        e.preventDefault()
-    })
+    input.addEventListener('invalid', preventDefault)
 })
 
-
+// Logic to show error message
 emailInputs.forEach((input)=> {
-    input.addEventListener("invalid", (e)=>{
-        e.preventDefault()
-    })
-    input.addEventListener("change", (e)=> {
-        if (e.target.value.match(emailRegex)) {
-            errorMessages.forEach((msg)=> {
-                msg.style.display = 'none'
-            })
-        }else {
-            errorMessages.forEach((msg)=> {
-                msg.style.display = 'block'
-            })
-        }
-    })
+    input.addEventListener("invalid", preventDefault)
+    input.addEventListener("change", showErrorMessage)
 })
 
 // Logic to toggle showing password
@@ -60,48 +54,129 @@ showPassword.forEach((btn)=> {
 
 // Logic to submit forms
 forms.forEach((form)=> {
-    form.addEventListener("submit", (e)=> {
-        e.preventDefault()
-    })
+    form.addEventListener("submit", preventDefault)
 })
 
+// Logic to change between sign-up and login forms
 mainButtons.forEach((mainButton) => {
-    mainButton.addEventListener("click", (e) => {
-        if (container.classList.contains("login")) {
-            loginContainer.classList.remove('active');
-            setTimeout(() => {
-                container.classList.remove('login');
-                container.classList.add('register');
-                loginContainer.style.display = 'none';
-                signupContainer.style.display = 'flex';
-                signupContainer.classList.add('active');
-                reset()
-            }, 300);
-        } else {
-            signupContainer.classList.remove('active');
-            setTimeout(() => {
-                container.classList.remove('register');
-                container.classList.add('login');
-                signupContainer.style.display = 'none';
-                loginContainer.style.display = 'flex';
-                loginContainer.classList.add('active');
-                reset()
-            }, 300);
-        }
-    });
+    mainButton.addEventListener("click", toggleForms);
 });
 
+// Logic to move to forget password form
 forgetPasswordButton.addEventListener('click', ()=> {
-    loginForm.innerHTML = ''
-    forgetPasswordDiv.style.display = "flex"
-    loginForm.append(forgetPasswordDiv)
-})
-sendCodeBtn.addEventListener('click', ()=> {
-    loginForm.innerHTML = ''
-    forgetPasswordDiv.style.display = "none"
-    loginForm.append(loginDiv)
+    render(forgetPasswordDiv)
 })
 
+// Logic to move to OTP form
+sendCodeBtn.addEventListener('click', renderOTPForm)
+
+// Logic to verify OTP and return to login form
+let otpValues = []
+otpButton.addEventListener('click', verifyOTP)
+
+// Logic to Switch color palette
+let currentPaletteIndex = 0;
+paletteSwitcher1.addEventListener('click', switchPalette);
+paletteSwitcher2.addEventListener('click', switchPalette);
+
+function showErrorMessage(e) {
+    if (e.target.value.match(emailRegex)) {
+        errorMessages.forEach((msg)=> {
+            msg.style.display = 'none'
+        })
+    }else {
+        errorMessages.forEach((msg)=> {
+            msg.style.display = 'block'
+        })
+    }
+}
+
+function preventDefault(e) {
+    e.preventDefault()
+}
+
+function toggleForms() {
+    if (container.classList.contains("login")) {
+        loginContainer.classList.remove('active');
+        setTimeout(() => {
+            container.classList.remove('login');
+            container.classList.add('register');
+            loginContainer.style.display = 'none';
+            signupContainer.style.display = 'flex';
+            signupContainer.classList.add('active');
+            reset()
+        }, 300);
+    } else {
+        signupContainer.classList.remove('active');
+        setTimeout(() => {
+            container.classList.remove('register');
+            container.classList.add('login');
+            signupContainer.style.display = 'none';
+            loginContainer.style.display = 'flex';
+            loginContainer.classList.add('active');
+            reset()
+        }, 300);
+    }
+}
+
+function renderOTPForm() {
+    if (!retriveEmailInput.value.match(emailRegex)) {
+        console.log("retrive-email isnot valid");
+    }else {
+        render(otpForm)
+        otpInputs[0].focus()
+    }
+}
+
+function verifyOTP() {
+    otpValues = []
+    otpInputs.forEach((input)=> {
+        otpValues.push(input.value)
+    })
+    if (!otpValues[0] || !otpValues[1] || !otpValues[2] || !otpValues[3]) {
+        otpValues = []
+        return false
+    }else {
+        render(loginDiv)
+    }
+}
+
+// Logic to check that OTP fields are only numbers
+function isNumberKey(e) {
+    let charCode = (e.which) ? e.which : e.keyCode;
+    if (charCode > 31 && (charCode < 48 || charCode > 57)) {
+        return false;
+    }
+    return true;
+}
+
+// Logic to auto focus next field in OTP form
+function moveFocus(currentInput, nextInputId) {
+    if (currentInput.value.length === 1) {
+        if (nextInputId !== null) {
+            document.getElementById(nextInputId).focus();
+        } else {
+            currentInput.blur();
+        }
+    }
+}
+
+// Function to switch palette
+function switchPalette() {
+    document.body.classList.remove(palettes[currentPaletteIndex]);
+    currentPaletteIndex = (currentPaletteIndex + 1) % palettes.length;
+    document.body.classList.add(palettes[currentPaletteIndex]);
+}
+
+// Render function to render the desired HTML elements
+function render(node) {
+    loginForm.innerHTML = ''
+    node.style.display = 'flex'
+    loginForm.append(node)
+    reset()
+}
+
+// Reset function to reset values to default
 function reset () {
     passwords.forEach((password)=> {
         password.type = 'password'
@@ -114,6 +189,10 @@ function reset () {
         msg.style.display = 'none'
     })
     allInputs.forEach((input)=> {
+        input.value = ''
+    })
+    retriveEmailInput.value = ''
+    otpInputs.forEach((input)=> {
         input.value = ''
     })
 }
